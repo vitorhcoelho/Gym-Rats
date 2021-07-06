225e40aa5ed0adc0394c96104a0de440df62a2cc
Vitor Hugo <vitor.hugo.cq@live.com>
2021-07-06 17:01:55 -0300

    listagem tela de treinos

--- lib/modules/training/view/training_tile.dart
+++ lib/modules/training/view/training_tile.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:gym_rats/models/treinos.dart';
 
+extension StringExtension on String {
+  String capitalize() {
+    return "${this[0].toUpperCase()}${this.substring(1)}";
+  }
+}
+
 class TreinoTile extends StatelessWidget {
   final Treinos treino;
 
@@ -11,137 +17,106 @@ class TreinoTile extends StatelessWidget {
     return Container(
       child: Padding(
         padding: EdgeInsets.all(20),
-        child: Container(
-          height: 140,
-          decoration: BoxDecoration(
-            color: Color(0xFF242424),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black,
-                offset: Offset(0.0, 1.0), //(x,y)
-                blurRadius: 6.0,
-              ),
-            ],
-          ),
+        child: Card(
+          shadowColor: Colors.black,
           child: Container(
-            height: 140,
+            height: 180,
             width: 50,
-            alignment: Alignment.center,
             color: Color(0xFF242424),
             child: ListView(
               physics: const NeverScrollableScrollPhysics(),
               children: [
                 Container(
                   color: Color(0xFFDF9F17),
-                  height: 35,
-                  width: 50,
-                  child: Align(
-                    alignment: AlignmentDirectional.centerEnd,
-                    child: IconButton(
-                      icon: Icon(Icons.delete),
-                      color: Colors.white,
-                      onPressed: () {
-                        showDialog(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          treino.dia.toString().capitalize(),
+                          textAlign: TextAlign.start,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                      Spacer(),
+                      IconButton(
+                        icon: Icon(Icons.delete),
+                        color: Colors.white,
+                        onPressed: () {
+                          showDialog(
                             context: context,
                             builder: (ctx) => AlertDialog(
-                                  title: Text('Excluir Treino'),
-                                  content: Text(
-                                      'Tem certeza que deseja excluir o treino?'),
-                                  actions: <Widget>[
-                                    MaterialButton(
-                                      child: Text('Não'),
-                                      onPressed: () {
-                                        Navigator.of(ctx).pop();
-                                      },
-                                    ),
-                                    MaterialButton(
-                                      child: Text('Sim'),
-                                      onPressed: () {
-                                        // Provider.of<Treinos>(context, listen: false)
-                                        //     .remove(treino);
-                                        // Navigator.of(ctx).pop();
-                                      },
-                                    ),
-                                  ],
-                                ));
-                      },
-                    ),
+                              title: Text('Excluir Treino'),
+                              content: Text(
+                                  'Tem certeza que deseja excluir o treino?'),
+                              actions: <Widget>[
+                                MaterialButton(
+                                  child: Text('Não'),
+                                  onPressed: () {
+                                    Navigator.of(ctx).pop();
+                                  },
+                                ),
+                                MaterialButton(
+                                  child: Text('Sim'),
+                                  onPressed: () {
+                                    // Provider.of<Treinos>(context, listen: false)
+                                    //     .remove(treino);
+                                    // Navigator.of(ctx).pop();
+                                  },
+                                ),
+                              ],
+                            ),
+                          );
+                        },
+                      ),
+                    ],
                   ),
                 ),
                 Container(
-                  color: Color(0xFF383838),
-                  height: 140,
+                  height: 180,
                   width: 50,
+                  color: Color(0xFF383838),
                   child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-                      Column(
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Row(
-                              children: [
-                                Text(
-                                  "Grupo Muscular: " + treino.grupoMuscular,
-                                  textAlign: TextAlign.center,
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Row(
-                              children: [
-                                Text(
-                                  "Nome do treino: " + treino.nome,
-                                  textAlign: TextAlign.center,
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Row(
-                              children: [
-                                Text(
-                                  "Repetições: " + treino.repeticoes.toString(),
-                                  textAlign: TextAlign.center,
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ],
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          "Grupo Muscular: " + treino.grupoMuscular,
+                          style: TextStyle(color: Color(0xFF979797)),
+                        ),
                       ),
-                      Column(
-                        children: [
-                          Row(
-                            children: [
-                              Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Row(
-                                  children: [
-                                    Text(
-                                      "Repetições: " +
-                                          treino.repeticoes.toString(),
-                                      textAlign: TextAlign.center,
-                                      style: TextStyle(
-                                        color: Colors.white,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ],
-                          )
-                        ],
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          "Nome do treino: " + treino.nome,
+                          style: TextStyle(
+                            color: Color(0xFF979797),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          "Repetições: " + treino.repeticoes.toString(),
+                          style: TextStyle(
+                            color: Color(0xFF979797),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          "Séries: " + treino.series.toString(),
+                          style: TextStyle(
+                            color: Color(0xFF979797),
+                          ),
+                        ),
                       ),
                     ],
                   ),
