88d2396a4dad8a8817b8cadd10e2a735da66a0ce
Vitor Hugo <vitor.hugo.cq@live.com>
2021-07-08 10:28:58 -0300

    importações nao utilizadas ignoradas

--- lib/models/treinosModel.dart
+++ lib/models/treinosModel.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unused_import
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:gym_rats/models/grupoMuscularGrafico.dart';
--- lib/modules/dashboard/components/grafico_grupomuscular.dart
+++ lib/modules/dashboard/components/grafico_grupomuscular.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: deprecated_member_use
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:charts_flutter/flutter.dart' as charts;
